a416146dad3a3bc3c3de98f95eb1e6e10155dc40
Anastasia Karyakina <anakaryakina@gmail.com>
2025-06-02 19:30:34 +0300

    Create modul Dialog, Notification and Toast

---
+++ Lesson2/notificationapp/src/main/java/com/mirea/karyakina/notificationapp/MainActivity.java
@@ -0,0 +1,62 @@
+package com.mirea.karyakina.notificationapp;
+
+import static android.Manifest.permission.POST_NOTIFICATIONS;
+
+import android.Manifest;
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
+import android.content.pm.PackageManager;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.app.NotificationCompat;
+import androidx.core.app.NotificationManagerCompat;
+import androidx.core.content.ContextCompat;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class MainActivity extends AppCompatActivity {
+    private static final String CHANNEL_ID = "com.mirea.asd.notification.ANDROID";
+    private String Tag = MainActivity.class.getSimpleName();
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_main);
+        if (ContextCompat.checkSelfPermission(this, POST_NOTIFICATIONS) ==
+                PackageManager.PERMISSION_GRANTED) {
+            Log.d(Tag, "Разрешения получены");
+        } else {
+            ActivityCompat.requestPermissions(this,
+                    new String[]{Manifest.permission.POST_NOTIFICATIONS}, 200);
+        }
+    }
+
+    public void onClickNewMessageNotification(View view){
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.POST_NOTIFICATIONS)
+                != PackageManager.PERMISSION_GRANTED) {
+            return;
+        }
+        NotificationCompat.Builder builder = new NotificationCompat.Builder(this, CHANNEL_ID)
+                .setContentText("Congratulation!")
+                .setSmallIcon(R.drawable.ic_launcher_foreground)
+                .setPriority(NotificationCompat.PRIORITY_HIGH)
+                .setContentTitle("MIREA");
+
+        int importance = NotificationManager.IMPORTANCE_DEFAULT;
+        NotificationChannel channel = new NotificationChannel(CHANNEL_ID,
+                "Student KaryakinaAO Notification ", importance);
+        channel.setDescription("MIREA Channel");
+
+        NotificationManagerCompat notificationManager = NotificationManagerCompat.from(this);
+        notificationManager.createNotificationChannel(channel);
+        notificationManager.notify(1, builder.build());
+    }
+
+}
